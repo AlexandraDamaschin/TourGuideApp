12c85e7ad76610035cb22f8ce1e20306f00093c6
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-04-08 11:21:16 +0100

    images added

--- app/src/main/java/com/example/android/tourguideapp/ListOfEvents.java
+++ app/src/main/java/com/example/android/tourguideapp/ListOfEvents.java
@@ -1,5 +1,22 @@
 package com.example.android.tourguideapp;
 
 
-public class ListOfEvents {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class ListOfEvents extends AppCompatActivity {
+    private static EventAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list);
+
+        //create a list of events
+        ArrayList<Event> events = new ArrayList<Event>();
+
+        events.add(new Event( R.drawable.alan_walker, "", "",));
+    }
 }
