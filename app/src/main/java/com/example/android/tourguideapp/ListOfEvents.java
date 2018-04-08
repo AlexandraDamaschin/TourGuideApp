d6e6583199dd72a1768b0052fcf0170a6e2ce2da
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-04-08 15:51:04 +0100

    clean up

--- app/src/main/java/com/example/android/tourguideapp/ListOfEvents.java
+++ app/src/main/java/com/example/android/tourguideapp/ListOfEvents.java
@@ -3,7 +3,6 @@ package com.example.android.tourguideapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
