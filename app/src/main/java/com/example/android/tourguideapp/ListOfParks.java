7fc182e854b91369903c68a6b5347bc31e27ba19
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-04-11 19:27:42 +0100

    list of parks as strings

--- app/src/main/java/com/example/android/tourguideapp/ListOfParks.java
+++ app/src/main/java/com/example/android/tourguideapp/ListOfParks.java
@@ -18,16 +18,16 @@ public class ListOfParks extends AppCompatActivity {
         //create a list of events
         ArrayList<Event> parks = new ArrayList<Event>();
 
-        parks.add(new Event(R.drawable.park, "Ardgillan", "Dublin"));
-        parks.add(new Event(R.drawable.park, "Ballycroy National Park", "Mayo"));
-        parks.add(new Event(R.drawable.park, "Coole Park", "Galway"));
-        parks.add(new Event(R.drawable.park, "Doneraile Park", "Cork"));
-        parks.add(new Event(R.drawable.park, "Glenveagh National Park ", "Donegal"));
-        parks.add(new Event(R.drawable.park, "Gougane Barra ", "Cork"));
-        parks.add(new Event(R.drawable.park, "Killarney National Park", "Kerry"));
-        parks.add(new Event(R.drawable.park, "Lough Boora", "Offaly"));
-        parks.add(new Event(R.drawable.park, "The Phoenix Park", "Dublin"));
-        parks.add(new Event(R.drawable.park, "St Stephen’s Green", ""));
+        parks.add(new Event(R.drawable.park, getResources().getString(R.string.park_1), getResources().getString(R.string.dublin)));
+        parks.add(new Event(R.drawable.park, getResources().getString(R.string.park_2), getResources().getString(R.string.mayo)));
+        parks.add(new Event(R.drawable.park, getResources().getString(R.string.park_3), getResources().getString(R.string.galway)));
+        parks.add(new Event(R.drawable.park, getResources().getString(R.string.park_4), getResources().getString(R.string.cork)));
+        parks.add(new Event(R.drawable.park, getResources().getString(R.string.park_5), getResources().getString(R.string.donegal)));
+        parks.add(new Event(R.drawable.park, getResources().getString(R.string.park_6), getResources().getString(R.string.cork)));
+        parks.add(new Event(R.drawable.park, getResources().getString(R.string.park_7), getResources().getString(R.string.kerry)));
+        parks.add(new Event(R.drawable.park, getResources().getString(R.string.park_8), getResources().getString(R.string.offlay)));
+        parks.add(new Event(R.drawable.park, getResources().getString(R.string.park_9), getResources().getString(R.string.dublin)));
+        parks.add(new Event(R.drawable.park, getResources().getString(R.string.park_10), getResources().getString(R.string.dublin)));
 
         ListView listView = findViewById(R.id.list);
         adapter = new EventAdapter(parks, getApplicationContext());
